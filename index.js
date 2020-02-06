5d51a8001a7cea1f03b838db3ede0ab9f484ed08
morinoko <forby.1@gmail.com>
2020-02-06 09:07:48 +0900

    remove unneeded code from old file

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 module.exports = Phrase;
 
-// Reverse a string
-function reverse(string) {
-  return Array.from(string).reverse().join('');
-}
-
 // Add reverse function to the String protoype to add 'reverse()' to all strings
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise
-// function palindrome(string) {
-//   let processedContent = string.toLowerCase();
-
-//   return processedContent === reverse(processedContent);
-// }
-
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
-
-  // Makes the phrase LOUDER
-  this.louder = function louder(argument) {
-    return this.content.toUpperCase();
-  }
 
   this.processor = function(string) {
     return string.toLowerCase();
@@ -40,17 +23,3 @@ function Phrase(content) {
     return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing (override method from Phrase)
-  this.processedContent = function processedContent() {
-    return this.processor(translation);
-  }
-}
-
-// Inherit the methods from Phrase
-TranslatedPhrase.prototype = new Phrase();

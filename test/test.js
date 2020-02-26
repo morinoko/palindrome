4f6bb5e06eebb3154b469aa4c400c8bd5a93ffda
morinoko <forby.1@gmail.com>
2020-02-27 08:47:00 +0900

    add test for letters method

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ let Phrase = require("../index.js");
 describe("Phrase", function() {
 
   describe("#palindrome", function() {
-
     it("should return false for a non-palindrome", function() {
       let nonPalindrome = new Phrase("apple");
       assert(!nonPalindrome.palindrome());
@@ -24,6 +23,13 @@ describe("Phrase", function() {
     it("should return true for a palindrome with punctuation", function() {
       let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
       assert(punctuatedPalindrome.palindrome());
+    });
+  });
+
+  describe("#letters", function() {
+    it("should return only letters", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
     });
   });
 });
